fe7979b73a1d7e4533a310c2e93059bcf31213c1
JulianLeClaire <753247@dpsk12.net>
2025-03-03 10:29:25 -0700

    Create feb 2 2025.cpp

---
+++ feb 2 2025.cpp
@@ -0,0 +1,24 @@
+#1
+def countdown_launch(n):
+    for i in range(n, 0, -1):
+        print(i, "...", end=" ")
+  print("lift off")
+
+
+countdown_launch(3)
+
+#2
+def has_digit(word):
+    for i in word:
+        if i=="1" or i=="2" or i=="3" or i=="4" or i=="5" or i=="6" or i=="7" or i=="8" or i=="9" or i=="0":
+                return "contains a number
+  return "noo nuymber found"
+print(has_digit("frog")
+print(has_digit("froggy1241")
+
+#3
+
+def trapeziod_area(b1, b2, h)
+  return ((b1*b2)*h)/2
+print(trapeziod_area(1, 2, 3)
+  
